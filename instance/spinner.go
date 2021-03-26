71d78132d9c0937170d7d8b4282d6e4c9c913b70
SumRundumDud <81445037+SumRundumDud@users.noreply.github.com>
2021-03-26 17:17:49 -0600

    Update spinner.go

--- instance/spinner.go
+++ instance/spinner.go
@@ -1,1 +1,13 @@
+package instance
 
+import (
+	"strings"
+
+	"github.com/dankgrinder/dankgrinder/discord"
+	"github.com/dankgrinder/dankgrinder/instance/scheduler"
+)
+
+func (in *Instance) spin(_ discord.Message) {
+	if !strings.Contains(in.sdlr.AwaitResumeTrigger(), "use spin") {
+		return
+	}
